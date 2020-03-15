fa8d9dfa795a358f0787a8799d5b3fda898d91ae
Regi Shehi <regi_shehi@hotmail.com>
2020-03-15 21:35:35 +0100

    Creating Resources using Post

--- TweetBook/Controllers/V1/PostsController.cs
+++ TweetBook/Controllers/V1/PostsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
 using TweetBook.Domain;
 
 namespace TweetBook.Controllers.V1
@@ -29,6 +30,24 @@ namespace TweetBook.Controllers.V1
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody]CreatePostRequest postRequest)
+        {
+            var post = new Post { Id = postRequest.Id };
+
+            if (string.IsNullOrEmpty(post.Id))
+                post.Id = Guid.NewGuid().ToString();
+
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{id}", post.Id);
+
+            var response = new Post { Id = postRequest.Id };
+
+            return Created(locationUri, response);
         }
     }
 }
