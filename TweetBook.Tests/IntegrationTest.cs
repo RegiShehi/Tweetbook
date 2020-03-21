72c91e4084fd8c3967e310834104c60212de6a14
Regi Shehi <regi_shehi@hotmail.com>
2020-03-21 21:48:20 +0100

    Add Integration Tests

---
+++ TweetBook.Tests/IntegrationTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Responses;
+using TweetBook.Data;
+
+namespace TweetBook.Tests
+{
+    public class IntegrationTest : IDisposable
+    {
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider _serviceProvider;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options => { options.UseInMemoryDatabase("TestDb"); });
+                    });
+                });
+
+            _serviceProvider = appFactory.Services;
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AutheticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "test@integration.com",
+                Password = "Password01!"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DataContext>();
+
+            context.Database.EnsureDeleted();
+        }
+    }
+}
---
+++ TweetBook.Tests/PostsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1;
+using TweetBook.Domain;
+
+namespace TweetBook.Tests
+{
+    [TestFixture]
+    public class PostsControllerTests : IntegrationTest
+    {
+        [Test]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            //arrange
+            await AutheticateAsync();
+
+            //act
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.GetAll);
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            CollectionAssert.IsEmpty(await response.Content.ReadAsAsync<List<Post>>());
+        }
+    }
+}
